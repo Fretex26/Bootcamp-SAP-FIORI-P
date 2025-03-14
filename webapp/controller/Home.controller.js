d260cc7fa906574118a4d563da9bc22dab8a5fa4
Freddy Alexander Barragán Rico <freb2060@gmail.com>
2025-03-14 02:54:50 +0000

    Get oData products

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,17 +1,21 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
 	"sap/m/MessageBox",
-	"sap/m/MessageToast"
-], (Controller, MessageBox) => {
+    "com/bootcamp/sapui5/freestyle/utils/HomeHelper"
+], (Controller, MessageBox, HomeHelper) => {
     "use strict";
 
     return Controller.extend("com.bootcamp.sapui5.freestyle.controller.Home", {
         onInit() {
         },
 
-        onPress:function () {
+        showName:function () {
             const sInputValue = this.byId('productInput').getValue()
             MessageBox.alert(`Hola ${sInputValue ? sInputValue : 'mundo'}`);
-		}
+		},
+
+        onPress:async function () {
+            let oDatos = await HomeHelper.getDataProducts()
+        }
     });
 });
--- webapp/utils/HomeHelper.js
+++ webapp/utils/HomeHelper.js
@@ -1,1 +1,16 @@
-sap
+sap.ui.define([
+    "com.bootcamp.sapui5.freestyle.freestyle.webapp.utils.HomeService"
+], function (HomeService) {
+    "use strict";
+
+    return{
+        init: function (oNorthwindModel) {
+            this._oNorthwindModel = oNorthwindModel
+        },
+
+        getDataProducts: async function () {
+            let oFilters = []
+            return HomeService.readProducts(this._oNorthwindModel, oFilters)
+        }
+    }
+})
