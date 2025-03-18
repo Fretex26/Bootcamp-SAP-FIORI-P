5020a9320cd1853b537adb7acf749f712cc95573
Freddy Alexander Barragán Rico <freb2060@gmail.com>
2025-03-18 15:39:11 +0000

    Vistas de Detalles de Ordenes

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,22 +1,23 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-	"sap/m/MessageBox",
+    "sap/m/MessageBox",
     "com/bootcamp/sapui5/freestyle/utils/HomeHelper",
-	'sap/ui/model/json/JSONModel'
+    'sap/ui/model/json/JSONModel'
 ], (Controller, MessageBox, HomeHelper, JSONModel) => {
     "use strict";
 
     return Controller.extend("com.bootcamp.sapui5.freestyle.controller.Home", {
         onInit() {
+            this.oRouter = this.getOwnerComponent().getRouter()
         },
 
-        showName:function () {
+        showName: function () {
             const sInputValue = this.byId('productInput').getValue()
             MessageBox.alert(`Hola ${sInputValue ? sInputValue : 'mundo'}`);
-            
-		},
 
-        searchProducts:async function () {
+        },
+
+        searchProducts: async function () {
             try {
                 let oDatos = await HomeHelper.getDataProducts();
                 let oModel = new JSONModel(oDatos[0]);
@@ -29,13 +30,20 @@ sap.ui.define([
 
         tableProducts: async function () {
             try {
-                let oDatos = await HomeHelper.getDataProducts();                
+                let oDatos = await HomeHelper.getDataProducts();
+                console.log('oDatos[0].results: ',oDatos[0].results);
+                
                 await HomeHelper.setProductModel(this, oDatos[0].results)
             } catch (error) {
                 MessageBox.error("Error al obtener productos para tabla.");
                 console.error(error);
             }
-        }
+        },
 
+        onItemPress: function (oEvent) {
+            let oSource = oEvent.getSource();
+            let oDatos = oSource.getBindingContext("ProductCollection").getObject();
+            this.oRouter.navTo("detail", { ProductID: oDatos.ProductID });
+        }
     });
 });
